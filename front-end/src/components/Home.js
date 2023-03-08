7ea4ca9bf8c4b964d8705462974f3f5886de36d0
nareshb7 <nareshbjava7@gmail.com>
2023-03-08 08:06:05 +0530

    Redux action added in home component

--- front-end/src/components/Home.js
+++ front-end/src/components/Home.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
+import { addUser } from '../redux/actions/Actions'
 
 const Home =()=> {
     const dispatch = useDispatch()
-    const state = useSelector(state=> state)
-    console.log(state, 'state 972==')
+    const data = useSelector(state=> state.data)
+    const handleClick =()=> {
+        dispatch(addUser(0))
+    }
     return (
         <div>
             <h1>Homee</h1>
-            <button onClick={handleClick}>Count : </button>
+            <button onClick={handleClick}>Count : {data.count}</button>
         </div>
     )
 }
