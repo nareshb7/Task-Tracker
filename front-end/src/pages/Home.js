d31bdf210acd62fce0a3b5a51766c749b283afc3
nareshb7 <nareshbjava7@gmail.com>
2023-04-19 15:24:10 +0530

    home page updated

--- front-end/src/pages/Home.js
+++ front-end/src/pages/Home.js
@@ -1,16 +1,19 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Button, Col, Row } from 'react-bootstrap'
 import { LinkContainer } from 'react-router-bootstrap'
 import './style/Home.css'
+import { UserContext } from '../App'
 
 const Home = () => {
+    const {currentUserVal} = useContext(UserContext)
     return (
         <Row>
             <Col md={6} className='d-flex flex-direction-column align-items-center justify-content-center'>
                 <div>
                     <h1>Welcome ...</h1>
                     <p>This is the Tracker Application, It will track your daily issue status</p>
-                    <div className='d-flex justify-content-around'>
+                    {
+                        !currentUserVal.fName && <div className='d-flex justify-content-around'>
                         <LinkContainer to='/login'>
                             <Button variant='success'>
                                 Login <i className='fas fa-comments home-message-icon'></i>
@@ -22,6 +25,8 @@ const Home = () => {
                             </Button>
                         </LinkContainer>
                     </div>
+                    }
+                    
                 </div>
             </Col>
             <Col md={6} className='home__bg'>
