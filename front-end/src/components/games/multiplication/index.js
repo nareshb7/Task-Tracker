5a3ee9c7844161dd7a962fd7c4cdb6a9c8865e6c
Naresh Baleboina <nareshbjava7@gmail.com>
2023-06-02 15:22:29 +0530

    multiplication game updated

--- front-end/src/components/games/multiplication/index.js
+++ front-end/src/components/games/multiplication/index.js
@@ -9,7 +9,8 @@ const MultiplicationGame = () => {
     const [num1, setNum1] = useState(generateNum())
     const [num2, setNum2] = useState(generateNum())
     const [score, setScore] = useState(JSON.parse(localStorage.getItem("score")) || 0)
-    const [userAns, setUserAns] = useState(null)
+    const [userAns, setUserAns] = useState('')
+    const [showAns,setShowAns] = useState(false)
     const [isCorrectAns, setIsCorrectAns] = useState(false)
 
     function updateLocalStorage() {
@@ -23,12 +24,14 @@ const MultiplicationGame = () => {
 
     const handleSubmit = (e) => {
         e.preventDefault()
+        setShowAns(true)
         const correctAnswer = num1 * num2
         if (userAns == correctAnswer) {
             setScore(s => s + 1)
             updateLocalStorage()
             generateNewNumbers()
             setIsCorrectAns(true)
+            setUserAns('')
         } else {
             score > 0 && setScore(s => s - 1)
             updateLocalStorage()
@@ -44,10 +47,10 @@ const MultiplicationGame = () => {
             <form class="form" id="form" onSubmit={handleSubmit}>
                 <h4 className="score" id="score">Score : {score}</h4>
                 <h2 className="question" id="question">What is {num1} multiply by {num2}?</h2>
-                <input type="text" className="input" id="input" placeholder="Enter your answer" onChange={(e) => setUserAns(e.target.value)} autofocus autocomplete="off"></input>
+                <input type="text" className="input" id="input" value={userAns} placeholder="Enter your answer" onChange={(e) => setUserAns(e.target.value)} autofocus autocomplete="off"></input>
                 <div className=' my-2 fw-bold fs-5'>
                     {
-                        userAns && <>
+                        showAns && <>
                             {isCorrectAns ?
                                 <span style={{ color: '#f0f' }}> Correct </span> : <span style={{ color: '#f00' }}>You are Mistaken!</span>
                             }
