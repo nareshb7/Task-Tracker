03a2cb346796320555415705515f23ec77713212
Naresh Baleboina <104125516+nareshb7@users.noreply.github.com>
2024-02-16 11:59:08 +0530

    Update Constants.js

--- front-end/src/components/utils/Constants.js
+++ front-end/src/components/utils/Constants.js
@@ -1,7 +1,8 @@
 const BE_URL1 = 'http://192.168.10.28:4040'
 const BE_URL2 = 'http://192.168.1.7:4040'
 const BE_URL3 = 'http://localhost:4040'
-export const BE_URL = BE_URL1
+const BE_URL4 = "https://task-tracker-one.onrender.com"
+export const BE_URL = BE_URL4
 export const emailpattern = /^[a-z][a-z.0-9]+@[a-z]+(?:[.][a-z]{2,})+$/
 export const mblPattern = /^[6,7,8,9][\d]{9}$/;
 export const psdPattern = /^(?=.*[0-9])(?=.*[A-Z])(?=.*[a-z])(?=.*[!@#$%&*<>]).{8,}$/
